7747e5ed84fa7af080f4f1e1d679e71ff050991c
DevJabr <muhammadmjabr@gmail.com>
2025-08-11 15:08:27 +0300

    changing reciters page

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,33 +4,31 @@ import QuranPic from "../../assets/QuranLogo.png";
 import { Link } from "react-router";
 const Navbar = () => {
   return (
-
-      <div className="nav">
-        <div className="right">
-          <Link to="/">
-            <img src={QuranPic} width={80} className="logo" />
-          </Link>
-        </div>
-
-        <div className="left">
-          <Link to="/">
-            <button>
-              <i class="fa-solid fa-radio"></i>
-            </button>
-          </Link>
-          <Link to="/recite">
-            <button>
-              <i class="fa-solid fa-headphones"></i>
-            </button>
-          </Link>
-          <Link to="https://quranapi.pages.dev/">
-            <button>
-              <i class="fa-solid fa-globe"></i>{" "}
-            </button>
-          </Link>
-        </div>
-
+    <div className="nav">
+      <div className="right">
+        <Link to="/">
+          <img src={QuranPic} width={80} className="logo" />
+        </Link>
       </div>
+
+      <div className="left">
+        <Link to="/">
+          <button>
+            <i class="fa-solid fa-podcast"></i>
+          </button>
+        </Link>
+        <Link to="/recite">
+          <button>
+            <i class="fa-solid fa-headphones"></i>
+          </button>
+        </Link>
+        <Link to="https://quranapi.pages.dev/">
+          <button>
+            <i class="fa-solid fa-globe"></i>{" "}
+          </button>
+        </Link>
+      </div>
+    </div>
   );
 };
 
